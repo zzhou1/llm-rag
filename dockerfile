c01ff66aee7199f3055f1c020b28fc3676a1bf8c
Rick Spencer <richard.linger.spencer.3@gmail.com>
2024-03-03 12:35:30 -0500

    stashing embeddings model in cache

--- dockerfile
+++ dockerfile
@@ -4,8 +4,9 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 RUN mkdir -p /models
-COPY rift-coder-v0-7b-q4_0.gguf /models
-
+COPY mistral-7b-openorca.gguf2.Q4_0.gguf /models
+RUN mkdir -p /root/.cache/gpt4all
+COPY all-MiniLM-L6-v2-f16.gguf /root/.cache/gpt4all
 COPY pdfs /pdfs
 
 COPY main.py .
--- main.py
+++ main.py
@@ -10,40 +10,40 @@ from langchain.callbacks.base import BaseCallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 import os
 
-embeddings = GPT4AllEmbeddings()
-
+print("set up embeddings")
+embeddings = GPT4AllEmbeddings(model="/models/all-MiniLM-L6-v2-f16.gguf")
+print("reading input")
 pdf_dir = 'pdfs'
 pdf_files = os.listdir(pdf_dir)
 
-all_texts = []
 documents = []
 for pdf_file in pdf_files:
     if pdf_file.endswith('.pdf'):
         print(f"-----------processing {pdf_file}")
         pdf_path = os.path.join(pdf_dir, pdf_file)  
         documents += PyPDFLoader(pdf_path).load_and_split()
+
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024,chunk_overlap=64)
 texts = text_splitter.split_documents(documents)
 
 faiss_index = FAISS.from_documents(texts, embeddings)
 
-# def get_embeddings():
-#     embedder = Embed4All()
-#     pdf_dir = 'pdfs'
-#     pdf_files = os.listdir(pdf_dir)
-#     embeddings = []
-#     for pdf_file in pdf_files:
-#         if pdf_file.endswith('.pdf'):
-#             print(f"-----------processing {pdf_file}")
-#             pdf_path = os.path.join(pdf_dir, pdf_file)
-            
-#             with open(pdf_path, 'rb') as file:
-#                 pdf_reader = PyPDF2.PdfReader(file)
-#                 pdf_text = ""
-#                 for page in pdf_reader.pages:
-#                     pdf_text += page.extract_text() + "\n"
-#                 output = embedder.embed(pdf_text)
-#                 embeddings.append(output)
-#     return embeddings
 
-# embeddings = get_embeddings()
+question = 'dental benefits'
+matched_docs = faiss_index.similarity_search(question, 4)
+context = ""
+for doc in matched_docs:
+    context = context + doc.page_content + " \n\n " 
+
+print(context)
+llm = GPT4All(model="/models/mistral-7b-openorca.gguf2.Q4_0.gguf", max_tokens=2000)
+
+template = """
+Please use the following context to answer questions.
+Context: {context}
+ - -
+Question: {question}
+Answer: Let's think step by step."""
+prompt = PromptTemplate(template=template, input_variables=["context", "question"]).partial(context=context)
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+print(llm_chain.invoke(question))

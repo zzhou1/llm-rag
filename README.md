87064f2e60394bc5ab0a41506e78cca29108e8c5
Rick Spencer <richard.linger.spencer.3@gmail.com>
2024-03-03 15:11:06 -0500

    added flask

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
-A project for knowledge embedding from a set of pdf files. It is designed so that the container will not need to use the network at all while building.
+A project for knowledge embedding from a set of pdf files. It is designed so that the container will not need to use the network at all while building (though it still uses pip install). The two main technologies in use are:
+ * langchain, for gluing everything together
+ * gpt4all for the models
 
-Very much a work in prorgress. Currently it will create all of the embeddings and run a single query.
+Very much a work in prorgress.
+
+I threw a terrible web app in front of it to facilitate playing around with the content. There is nothing fancy like remember context or anything like that yet, but it shouldn't be too hard to add, since I believe that is all built into langchain.
 
 # setup
 1. create a folder called pdfs, and put the pdfs that you want to index in there.
 2. Download mistral-7b-openorca.gguf2.Q4_0.gguf and all-MiniLM-L6-v2-f16.gguf from gpt4all and store them in the root directory.
 
 # build and run
-
 ```
 sudo docker build -t llm .
-sudo docker run -a stdin -a stdout -t -i llm /bin/bash
-#python main.py
+sudo docker run -p 5001:5001 llm
 ```
+
+Then wait until the web app is ready. You will see this:
+```bash
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on all addresses (0.0.0.0)
+ * Running on http://127.0.0.1:5001
+ * Running on http://172.17.0.2:5001
+Press CTRL+C to quit
+```
+In your web browser go to localhost:5001. Enter a query, and then wait for the answer. You can use the back button to enter another query. 
 
--- main.py
+++ main.py
@@ -6,39 +6,55 @@ from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 import os
-
-embeddings = GPT4AllEmbeddings(model="/models/all-MiniLM-L6-v2-f16.gguf")
-pdf_dir = 'pdfs'
-pdf_files = os.listdir(pdf_dir)
-
-print("reading input files")
-documents = []
-for pdf_file in pdf_files:
-    if pdf_file.endswith('.pdf'):
-        print(f"    {pdf_file}")
-        pdf_path = os.path.join(pdf_dir, pdf_file)  
-        documents += PyPDFLoader(pdf_path).load_and_split()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024,chunk_overlap=64)
-texts = text_splitter.split_documents(documents)
-
-faiss_index = FAISS.from_documents(texts, embeddings)
+from flask import Flask, render_template, request
 
 
-question = 'is routine cleaning covered in dental benefits?'
-matched_docs = faiss_index.similarity_search(question, 4)
-context = ""
-for doc in matched_docs:
-    context = context + doc.page_content + " \n\n " 
 
-llm = GPT4All(model="/models/mistral-7b-openorca.gguf2.Q4_0.gguf", max_tokens=2000)
+def set_up_embeddings():
+    embeddings = GPT4AllEmbeddings(model="/models/all-MiniLM-L6-v2-f16.gguf")
 
-template = """
-Please use the following context to answer questions.
-Context: {context}
- - -
-Question: {question}
-Answer: Let's think step by step."""
-prompt = PromptTemplate(template=template, input_variables=["context", "question"]).partial(context=context)
-llm_chain = LLMChain(prompt=prompt, llm=llm)
-print(llm_chain.invoke(question))
+    pdf_dir = 'pdfs'
+    pdf_files = os.listdir(pdf_dir)
+
+    print("reading input files")
+    documents = []
+    for pdf_file in pdf_files:
+        if pdf_file.endswith('.pdf'):
+            print(f"    {pdf_file}")
+            pdf_path = os.path.join(pdf_dir, pdf_file)  
+            documents += PyPDFLoader(pdf_path).load_and_split()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024,chunk_overlap=64)
+    texts = text_splitter.split_documents(documents)
+    return FAISS.from_documents(texts, embeddings)
+
+faiss_index = set_up_embeddings()
+app = Flask(__name__)
+
+@app.route('/render', methods=['POST'])
+def invoke():
+    question = request.form["input"]
+    matched_docs = faiss_index.similarity_search(question, 4)
+    context = ""
+    for doc in matched_docs:
+        context = context + doc.page_content + " \n\n " 
+
+    llm = GPT4All(model="/models/mistral-7b-openorca.gguf2.Q4_0.gguf", max_tokens=2000)
+
+    template = """
+    Please use the following context to answer questions.
+    Context: {context}
+    - -
+    Question: {question}
+    Answer: Let's think step by step."""
+    prompt = PromptTemplate(template=template, input_variables=["context", "question"]).partial(context=context)
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    answer = llm_chain.invoke(question)
+    return render_template('answer.html', question=question, answer=answer['text'], context=context)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
